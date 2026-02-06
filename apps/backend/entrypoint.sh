2248bff40c1431abd3b7edf4245070467039b106
Davie Karanja <80883320+daviekaranja@users.noreply.github.com>
2026-02-06 17:49:38 +0300

    fixed path for gunicorn

--- apps/backend/entrypoint.sh
+++ apps/backend/entrypoint.sh
@@ -7,4 +7,4 @@ alembic upgrade head
 echo "Starting Gunicorn..."
 # main:app is correct because main.py is in the WORKDIR (/app)
 # app.lib imports will work because the 'app' folder is also in /app
-exec gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000
+exec gunicorn -w 2 -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8000
